6f2c3aaedf3b6cb93f1c382c354bc8bfd32b78b9
Brandon Rafael Lovelyno <brandonrafael80@gmail.com>
2024-11-13 09:35:34 +0700

    feat: connecting database

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
+// Importing supporting packages
 const morgan = require("morgan");
 const helmet = require("helmet");
 
+// Implementing supporting packages
 const express = require("express");
 const app = express();
 app.use(express.json());
 app.use(morgan("dev"));
 app.use(helmet());
 
+// Connecting the database
+const connectDB = require("./config/database");
+connectDB(process.env.MONGO_URI);
+
+// Implementing the main router
 const makeMainRouter = require("./routes");
 const mainRouter = makeMainRouter();
 
