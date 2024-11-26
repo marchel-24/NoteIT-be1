394821222e361f9c2f502a2a82393b9a7acbd623
Brandon <brandonrafael80@gmail.com>
2024-11-26 08:26:17 +0700

    feat: using cors

--- app.js
+++ app.js
@@ -9,6 +9,17 @@ app.use(express.json());
 app.use(morgan("dev"));
 app.use(helmet());
 
+// Allowing cross origin request
+const cors = require("cors");
+app.use(
+  cors({
+    origin: "*",
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    allowedHeaders: ["Content-Type", "Authorization"],
+    credentials: true,
+  })
+);
+
 // Connecting the database
 const connectDB = require("./config/database");
 connectDB(process.env.MONGO_URI);
