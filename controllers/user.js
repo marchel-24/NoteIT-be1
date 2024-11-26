ff4b7a9ff2ebdffd7a44342af366f110e1e71bb0
gabriel <gabrielsyailendraf@mail.ugm.ac.id>
2024-11-26 22:37:55 +0700

    add session

--- controllers/user.js
+++ controllers/user.js
@@ -160,3 +160,34 @@ exports.googleCallback = async (req, res) => {
     },
   });
 };
+
+exports.session = async (req, res) => {
+  const token = req.cookies.jwt;
+
+  if (!token) {
+    return res.status(403).json({
+      message: "missing credential",
+    });
+  }
+
+  const decode = jwt.verify(token, process.env.JWT_SECRET_KEY);
+  if (!decode) {
+    return res.status(400).json({
+      status: "invalid",
+    });
+  }
+
+  const user = await User.findById(decode);
+
+	if (!user) {
+		return res.status(400).json({
+      status: "invalid",
+    });
+	}
+
+  return res.status(200).json({
+		id: user._id
+	});
+
+  next();
+};
