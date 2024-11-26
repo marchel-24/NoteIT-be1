a726a861e80f514433137f35b2849095ad889d14
Brandon <brandonrafael80@gmail.com>
2024-11-26 19:11:35 +0700

    feat: append profile picture for user

--- models/user.js
+++ models/user.js
@@ -1,21 +1,24 @@
 const mongoose = require("mongoose");
-const bcrypt = require('bcryptjs');
+const bcrypt = require("bcryptjs");
 
 const userSchema = new mongoose.Schema({
   email: {
     type: String,
     required: true,
-    unique: true
+    unique: true,
   },
   password: {
     type: String,
-  }
+  },
+  profilePicture: {
+    type: String,
+  },
 });
 
-userSchema.pre('save', async function (next) {
+userSchema.pre("save", async function (next) {
   const user = this;
 
-  if (!user.isModified('password')) return next(); 
+  if (!user.isModified("password")) return next();
   try {
     const salt = await bcrypt.genSalt(12);
     user.password = await bcrypt.hash(user.password, salt);
